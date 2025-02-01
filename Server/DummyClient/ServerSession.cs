bb560bbe2591518e86b991a95e66a1531e48ef62
Kangho Dean Kim <88331311+ariaskkh@users.noreply.github.com>
2025-02-01 22:08:47 +0900

    Serialization #1

--- Server/DummyClient/ServerSession.cs
+++ Server/DummyClient/ServerSession.cs
@@ -10,25 +10,58 @@ class Packet
     public ushort packetId; // packet 종류 구분
 }
 
+class PlayerInfoReq : Packet
+{
+    public long playerId;
+}
+
+class PlayerInfoOk : Packet
+{
+    public int hp;
+    public int attack;
+}
+
+public enum PacketID
+{ 
+    PlayerInfoReq = 1,
+    PlayerInfoOk = 2,
+}
+
 class ServerSession : Session
 {
+    // TryWriteBytes 이외 방법: C++ 같이 짜면 속도가 훨씬 빨라짐. 현재 사용하지 않음
+    //static unsafe void ToBytes(byte[] array, int offset, ulong value)
+    //{
+    //    fixed (byte* ptr = &array[offset])
+    //        *(ulong*)ptr = value;
+    //}
+
     public override void OnConnected(EndPoint endPoint)
     {
         Console.WriteLine($"OnConnected: {endPoint}");
 
-        for (int i = 0; i < 5; i++)
+        PlayerInfoReq packet = new() { packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
+
+        //for (int i = 0; i < 5; i++)
         {
             // 보낸다
-            Packet packet = new() { size = 4, packetId = 7 };
+            ArraySegment<byte> seg = SendBufferHelper.Open(4096);
+            
+            ushort count = 0;
+            bool success = true;
 
-            ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-            byte[] buffer = BitConverter.GetBytes(packet.size);
-            byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-            Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
-            Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
-            ArraySegment<byte> sendBuff = SendBufferHelper.Close(packet.size);
+            //success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset, seg.Count), packet.size);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset + count, seg.Count - count), packet.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset + count, seg.Count - count), packet.playerId);
+            count += 8;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset, seg.Count), count); // size를 마지막에 넣어줘야 함
+            
+            ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
 
-            Send(sendBuff);
+            if (success)
+                Send(sendBuff);
         }
     }
 
--- Server/Server/ClientSession.cs
+++ Server/Server/ClientSession.cs
@@ -9,6 +9,23 @@ class Packet
     public ushort packetId; // packet 종류 구분
 }
 
+class PlayerInfoReq : Packet
+{
+    public long playerId;
+}
+
+class PlayerInfoOk : Packet
+{
+    public int hp;
+    public int attack;
+}
+
+public enum PacketID
+{
+    PlayerInfoReq = 1,
+    PlayerInfoOk = 2,
+}
+
 class ClientSession : PacketSession
 {
     public override void OnConnected(EndPoint endPoint)
@@ -19,17 +36,17 @@ class ClientSession : PacketSession
         // [ 100 ][ 10 ]
         //byte[] sendBuff = new byte[4096];
 
-        ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-        byte[] buffer = BitConverter.GetBytes(packet.size);
-        byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-        Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
-        Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
-        ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+        //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+        //byte[] buffer = BitConverter.GetBytes(packet.size);
+        //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
+        //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+        //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+        //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
 
-        // 100명
-        // 1 -> 이동패킷이 100명
-        // 100 -> 이동패킷이  100 * 100 = 1만
-        Send(sendBuff);
+        //// 100명
+        //// 1 -> 이동패킷이 100명
+        //// 100 -> 이동패킷이  100 * 100 = 1만
+        //Send(sendBuff);
         Thread.Sleep(1000);
         Disconnect();
     }
@@ -37,8 +54,23 @@ class ClientSession : PacketSession
     // 딱 유효한 부분만 받음
     public override void OnRecvPacket(ArraySegment<byte> buffer)
     {
-        ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-        ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + 2);
+        ushort count = 0;
+        ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+        count += 2;
+        ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+        count += 2;
+
+        switch ((PacketID)id)
+        {
+            case PacketID.PlayerInfoReq:
+                {
+                    long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);
+                    count += 8;
+                    Console.WriteLine($"PlayerInfoReq: {playerId}");
+                }
+                break;
+        }
+
         Console.WriteLine($"RecvPacketId: {id}, Size: {size}");
     }
 
